78792697bc93611811fdeddd42546386fd21d2a2
bibhu-git <bibhuranjanm324@gmail.com>
2026-04-02 03:54:55 +0530

    modify vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://patient-management-system-sgri.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
 });
