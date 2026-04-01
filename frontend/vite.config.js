64863b1fb766bc5edda58dd8d0a11d4e97bebacc
bibhu-git <bibhuranjanm324@gmail.com>
2026-04-02 03:32:38 +0530

    modify backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://patient-management-system-sgri.onrender.com',
         changeOrigin: true,
       },
     },
